88b75f257111bc01fe173b681390cc1e54acd8bb
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2019-01-12 22:42:31 -0500

    Page layout changes, among other things...

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/css/index.css", function(req, res) {
   res.sendFile(__dirname + "/css/index.css");
 });
 
+app.get("/css/bootstrap.min.css", function(req, res) {
+  res.sendFile(__dirname + "/css/bootstrap.min.css");
+});
+
 app.get("/fonts/Gravity-Book.otf", function(req, res) {
   res.sendFile(__dirname + "/fonts/Gravity-Book.otf");
 });
@@ -24,10 +28,29 @@ app.get("/fonts/Gravity-Light.otf", function(req, res) {
 
 io.on("connection", function(socket) {
   console.log("User: " + socket.id + " has connected");
+  let id = socket.id;
+  socket.emit("sendId", id);
+  let password = genPassword();
+  socket.emit("sendPassword", password);
+
   socket.on("keyPressed", function(e) {
     console.log(e + " key pressed");
+    socket.on("dataReceived", function(data, fn) {
+      fn(data);
+    });
   });
 });
+
+function genPassword() {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+
+  for (var i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 http.listen(8000, function() {
   console.log("listening on *:8000");
--- socket.js
+++ socket.js
@@ -2,11 +2,32 @@ const socket = io();
 
 var partnerTextArea = document.getElementById("partnerTextArea");
 var clientTextArea = document.getElementById("clientTextArea");
+var code = [];
+
+socket.on("connection", function(socket) {
+  console.log("hi");
+  //let p = document.getElementById("clientId");
+  //let id = newsocket.socket.sessionid;
+  //console.log(id);
+});
+
+socket.on("sendId", function(id) {
+  let p = document.getElementById("clientId");
+  p.innerHTML += " " + id;
+});
+
+socket.on("sendPassword", function(password) {
+  let p = document.getElementById("clientPassword");
+  p.innerHTML += " " + password;
+});
 
 document.addEventListener("keydown", keyCheck);
 function keyCheck(e) {
+  code.push(e.key);
+  //socket.emit("dataReceived", code, function(data) {
+  //createTextArea(data);
+  //});
   let char = String.fromCharCode(e.keyCode);
-
   switch (e.keyCode) {
     case 8:
       partnerTextArea.value = clientTextArea.value.slice(
@@ -46,4 +67,28 @@ function keyCheck(e) {
   socket.emit("keyPressed", e.key, function(error, message) {
     console.log(error + ": " + message);
   });
+}
+
+function createTextArea(data) {
+  let parent = document.getElementById("partnerCode");
+  element = document.getElementById("partnerTextArea");
+  element.parentNode.removeChild(element);
+
+  let newElement = document.createElement("textarea");
+  newElement.id = "partnerTextArea";
+  newElement.classList.add("textarea");
+  newElement.disabled = true;
+  newElement.readOnly = true;
+  let text = "";
+  for (let i = 0; i < data.length; i++) {
+    text += data[i];
+  }
+  console.log(text);
+  newElement.value = text;
+  parent.append(newElement);
+}
+
+function showConnectForm() {
+  console.log("hi");
+  document.getElementById("connectForm").style.display = "block";
 }
